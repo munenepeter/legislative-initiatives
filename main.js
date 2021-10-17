1b02b43a39d371057f00d58f40f9729dc44755f5
Peter Munene <peternjega7@gmail.com>
2021-10-17 19:47:48 +0300

    not working

--- main.js
+++ main.js
@@ -1,26 +1,41 @@
 class Data {
-    data = [];
+
+    data;
+
     constructor(url) {
         this.url = url;
-
     }
     get lis() {
-        return this.getLis();
-    }
-    getLis() {
-        fetch(this.url)
-            .then(response => response.json())
-            .then((lis) => {
-               console.log(lis)
-                 lis = this.data;
-            });
+        this.data = this.getLis();
         return this.data;
     }
+    get count() {
+        //return the count of all LI's
+        return 7;
+    }
+     getLis() {
+        // fetch(this.url)
+        //     .then(response => response.json())
+        //     .then((lis) => {
+        //         // console.log(lis)
+        //         //lis = this.data;
+        //         this.data = lis;
+        //         //console.log(this.data)
+        //         return this.data;
+        //     });
+         let d = fetch(this.url)
+             .then((response) => { return response.json() });
+         console.log(d);
+        
+        
+    }
+
 }
 
 let data = new Data("http://127.0.0.1:5500/data/datas.json");
 
-console.log(data.lis())
+console.log(data.data);
+data.getLis();
 
 
 
